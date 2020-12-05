f95c3ff3281e5a0db86c5529e3d6ed6abacf0cf3
hiimharry <hallexlol@gmail.com>
2020-12-05 01:30:29 -0500

    further improved some code

--- day5/day5-improved.py
+++ day5/day5-improved.py
@@ -2,31 +2,25 @@ f = open("input.txt", "r")
 
 seats = f.read().split("\n")
 
-#alternative method part 1
+#part 1
 def getSeatID(seat):
-    id = 0
+    col = 0
     row = 0
     for x in range(7):
         if seat[x] == "B":
             row += 2 ** (6-x)
-    id += row * 8
-    col = 0
     for x in range(3):
         if seat[x+7] == "R":
             col += 2 ** (2-x)
-    id += col
-    return id
+    return row * 8 + col
 
 currMax = 0
 for s in seats:
     currMax = max(currMax, getSeatID(s))
-
-
 print(currMax)
 
 #part 2
-seats = [getSeatID(s) for s in seats]
-seats = sorted(seats)
+seats = sorted([getSeatID(s) for s in seats])
 for x in range(currMax):
     if x in seats and not x+1 in seats:
         print(x+1)
